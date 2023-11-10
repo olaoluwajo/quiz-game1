d9f1c45ea2be22d219613e542e23bc694afb2908
olaoluwa <146534182+olaoluwajo@users.noreply.github.com>
2023-11-10 19:50:12 +0100

    update

--- js/quiz.js
+++ js/quiz.js
@@ -1,15 +1,13 @@
-// let correct = 0;
-// let completed = 0;
+let correct = 0;
+let completed = 0;
 
-// let fact;
+let fact;
 
 //  Declare & assign variables pointing to the corresponding element(s)
 const nextButton = document.getElementById("next-question");
-// const statement = document.getElementById("statement");
 const optionButtons = document.querySelector("#options").children;
 const explanation = document.getElementById("explanation");
 
-// TODO 2: Declare & assign a variable called fact
 // Its value should be an object with a statement, true/false answer, and explanation
 const facts = [
   {
@@ -121,35 +119,3 @@ for (let option of optionButtons) {
 }
 
 getNextFact();
-// // statement.textContent = fact.statement;
-
-// // const disable = (button) => {
-// //   button.setAttribute("disabled", "");
-// // };
-
-// // const enable = (button) => button.removeAttribute("disabled");
-
-// // TODO 5: Declare an isCorrect function that compares a guess to the right answer
-
-// function isCorrect(guessString) {
-//   return guessString === fact.answer.toString();
-// }
-
-// // TODO 6A: Use a for loop to add a click event listener to each of the optionButtons
-
-// for (let button of optionButtons) {
-//   button.addEventListener("click", (e) => {
-//     explanation.textContent = fact.explanation;
-
-//     for (let otherButton of optionButtons) {
-//       disable(otherButton);
-//     }
-//     // TODO 8: Within the event handler function,
-//     // Get the guessed value from the clicked button
-//     if (isCorrect(button.value)) {
-//       button.classList.add("correct");
-//     } else {
-//       button.classList.add("incorrect");
-//     }
-//   });
-// }
